6944bae73021ceaffb8d56d980ac6efa46d5f952
Abdi Berhe <abdi201380@gmail.com>
2023-09-27 19:35:50 +0000

    [Update]: Confirm with eslint

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,15 +1,9 @@
-// 3-all.js
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  // Use Promise.all to resolve all promises
-  Promise.all([uploadPhoto(), createUser()])
-    // Log the body, firstName, and lastName to the console
-    .then((values) => {
-      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
-    // Log an error message to the console
-    .catch(() => {
-      console.error('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
