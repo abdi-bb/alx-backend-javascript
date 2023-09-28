25dd457fca7142ccdbd1ca12c43b781d897e769b
Abdi Berhe <abdi201380@gmail.com>
2023-09-28 11:25:41 +0000

    [Update]: The returned promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,18 +4,14 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   // Call the signUpUser and uploadPhoto functions and store the promises in an array
-  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-  // Return a new Promise object
-  return new Promise((resolve) => {
-    // Use Promise.allSettled to wait for all the promises to be settled
-    Promise.allSettled(promises).then((results) => {
-      // Map the results array to an array of objects with status and value attributes
-      const finalResults = results.map((result) => ({
-        status: result.status,
-        value: result.value || result.reason,
-      }));
-      // Call the resolve function with the finalResults array
-      resolve(finalResults);
-    });
+  return Promise.allSettled(
+    [signUpUser(firstName, lastName),
+      uploadPhoto(fileName)],
+  ).then((values) => {
+    const arr = [];
+    for (const item of values) {
+      arr.push({ status: item.status, value: item.value || item.reason });
+    }
+    return arr;
   });
 }
