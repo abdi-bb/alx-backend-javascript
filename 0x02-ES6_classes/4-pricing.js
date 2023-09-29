5514f1414fe88f32c126d39332e34bb4ca48bacc
Abdi Berhe <abdi201380@gmail.com>
2023-09-29 18:56:43 +0000

    [Update]: Add methods displayFullPrice and convertPrice

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -3,7 +3,9 @@ import Currency from './3-currency';
 export default class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
-    this._currency = currency;
+    if (currency instanceof Currency) {
+      this._currency = currency;
+    }
   }
 
   get amount() {
