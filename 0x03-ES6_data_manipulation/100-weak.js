1454b408446e1a5b075c6c106e9baf29f0228134
Abdi B <abdi201380@gmail.com>
2023-10-02 20:15:00 -0500

    [Add]: WeakMap

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,13 +1,12 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 1);
+  let count = weakMap.get(endpoint) || 0;
+  count += 1;
+
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
   } else {
-    const count = weakMap.get(endpoint);
-    if (count >= 5) {
-      throw new Error('Endpoint load is high');
-    }
-    weakMap.set(endpoint, count + 1);
+    weakMap.set(endpoint, count);
   }
 }
