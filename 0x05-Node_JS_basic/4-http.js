bbc6656d191bb55d5d7f38b2d1e702a0e26d7166
Abdi Berhe <abdiberhe@gmail.com>
2023-11-21 21:45:16 +0300

    [Server]: Small http server using node's http module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.end('Hello Holberton School!');
+});
+
+const PORT = 1245;
+
+app.listen((PORT), () => {
+  console.log(`Server running on port ${PORT}...`);
+});
+
+module.exports = app;
